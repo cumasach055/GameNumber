4f878b7f10da64266b7bb8f6dd42527e1c245f1b
cumasach055 <alishersarsenbay8@gmail.com>
2019-10-02 15:05:01 +0200

    my Hw2

---
+++ app.js
@@ -0,0 +1,17 @@
+let x = 17;
+/*
+*/
+
+ var question = prompt('Введите число');
+
+
+ if (question < 17 ) {
+ 	alert('Маленькое число')
+
+ } else if (question > 17) {
+ 	alert('Слишком большое число')
+ } else if (question == x) {
+ 	alert('Введен правильно')
+ }	else {
+ 	alert("Число не введен")
+ }
